384afad2c95ef4053e023a2888414a4936c76c77
RostislavKlech <KlechRostislav@seznam.cz>
2024-10-09 11:58:54 +0200

    Update ANSWER_5.sql

--- ANSWER_5.sql
+++ ANSWER_5.sql
@@ -1,5 +1,5 @@
 -- 5. Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, 
--- projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+-- projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
 
 -- KORELACE
 
